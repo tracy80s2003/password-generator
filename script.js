9c44c069d10672d5596b04e878a13118f0356df9
Tracy Golden <tracy80s2003@gmail.com>
2020-11-21 08:52:37 -0600

    added JS and console.log prompt

--- script.js
+++ script.js
@@ -1,4 +1,32 @@
 // Assignment Code
+var lowerCaseChar = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+
+var upperCaseChar = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+
+var numericNumber = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+
+var specialChar = ['#', '$', '%', '&', '(', ')', '*', '+', ',', '-', '.', '/', ':', ';', '<', '=', '>', '?', '@', '['];
+
+var charCount = parseInt(prompt('Enter a password between 8 and 128 characters'))
+
+if (charCount >= 8 && charCount <= 128) {
+  var finalChars = []
+
+  var includeSpecial = confirm('Password should include special characters?')
+  var includeNumbers = confirm('Password should include numbers?')
+
+  if (includeNumbers) {
+
+  }
+
+  if (includeNumbers) {
+
+  }
+
+  console.log(finalChars)
+
+}
+
 var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
