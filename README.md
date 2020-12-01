7d75f00c494813b151e3afc1cfe046972145f3be
Tracy Golden <tracy80s2003@gmail.com>
2020-11-30 21:07:35 -0600

    updated js, html and readme files

--- README.md
+++ README.md
@@ -1,6 +1,18 @@
 # password-generator
 
-# user story
+# User story
 As an employee with access to sensitive data we need to be able to randomly generate a password that meets certain criteria, so that we can create a strong password to provide greater security.
+
+## Rules
+To obtain a random and secure password we will click the red "Generate Password" button. Next, prompts will walk us through all of the password criteria options where we can either click "Cancel" to ignore/exclude/move past that prompt or "OK" to accept/include/confirm that prompt. 
+
+The first prompt asks to confirm choice of character length (between 8-128 characters). If number is < 8 or > 128 an alert will be shown to "Try Again." Once the length has been accepted prompts will ask to include/exclude the following criteria: uppercase letters, lowercase letters, special characters and numbers. Once through all of the prompts, a password value will be generated within the <textarea>.
+
+## Features
+Updated text on password generator homepage and added color to the text "red" to match the button background-color.
+
+see snapshot for feature:
+<img src="./password-generator/assets/images/passwordGen_red_screenshot.png" />
+
 
 https://tracy80s2003.github.io/password-generator/
--- script.js
+++ script.js
@@ -1,28 +1,49 @@
-// Assignment Code
+// Variables for password characters
+var lowerCaseChar = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
+var upperCaseChar = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
+var numericNumber = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+var specialChar = ['#', '$', '%', '&', '(', ')', '*', '+', ',', '-', '.', '/', ':', '<', '=', '>', '?', '@', '[', '~', '|', '{', '}', ' ', '"'];
 
-// declaring all variables for password characters
+// Add event listener to generate button
+document.getElementById("startBtn").addEventListener('click', myFunction);
 
-var charCount = ["lowerCaseChar", "upperCaseChar", "numericNumber", "specialChar",]
-  lowerCaseChar = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
-  upperCaseChar = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
-  numericNumber = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
-  specialChar = ['#', '$', '%', '&', '(', ')', '*', '+', ',', '-', '.', '/', ':', '<', '=', '>', '?', '@', '[', '~', '|', '{', '}', ' ', '"']
-  
-  // var strLength=str.length;
+function myFunction() {
 
   var charCount = parseInt(prompt('Please enter a password between 8 and 128 characters long.'))
 
   if (charCount >= 8 && charCount <= 128) { 
-    var includeLowerUpperCaseChar = parseInt(prompt('Does password include uppercase letters?'))
-    var includeLowerCaseChar = parseInt(prompt('Does password include lowercase letters?'))
-    var includeSpecialChar = parseInt(prompt('Does password include special characters?'))
-    var includeNumericNumbers = parseInt(prompt('Does password include numbers?'))
+    var includeUpperCaseChar = confirm('Does password include uppercase letters?');
+    var includeLowerCaseChar = confirm('Does password include lowercase letters?');
+    var includeSpecialChar = confirm('Does password include special characters?');
+    var includeNumericNumbers = confirm('Does password include numbers?');
+
+    var charBank = [];
+
+    if (includeLowerCaseChar == true){
+      charBank = charBank.concat(lowerCaseChar);
+    }
+    if (includeUpperCaseChar){
+      charBank = charBank.concat(upperCaseChar);
+    }
+    if (includeSpecialChar){
+      charBank = charBank.concat(specialChar);
+    }
+    if (includeNumericNumbers){
+      charBank = charBank.concat(numericNumber);
+    }
+
+    var randomPass = "";
+
+    for(var i = 0; i<charCount ; i++){
+      var newRandChar = charBank[Math.floor(Math.random() * charBank.length)];
+      randomPass += newRandChar;
+    }
+
+    document.getElementById("password").value = randomPass;
+
+  }else{
+    alert("We only handle a length of 8 through 128 characters. Please try again!")
+    myFunction()
   }
 
-// Add event listener to generate button
-//generateBtn.addEventListener("click", writePassword);
-document.getElementById("click").addEventListener('button', myFunction);
-
-function myFunction() {
-  document.getElementById("click").innerHTML = parseInt(prompt);
 }
